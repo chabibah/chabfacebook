e67ce98638a75b86f560fec68f7ee73c93c81566
chabibah <48429009+chabibah@users.noreply.github.com>
2019-06-28 13:51:36 +0700

    Create PostsFromPageExtractor.java

---
+++ PostsFromPageExtractor.java
@@ -0,0 +1,59 @@
+import facebook4j.Comment;
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.PagableList;
+import facebook4j.Post;
+import facebook4j.Reading;
+import facebook4j.ResponseList;
+import facebook4j.auth.AccessToken;
+
+public class PostsFromPageExtractor {
+
+/*
+
+A simple Facebook4J client which
+illustrates how to access group feeds / posts / comments.
+@param args
+@throws FacebookException 
+*/
+public static void main(String[] args) throws FacebookException {
+
+ // Generate facebook instance.
+Facebook facebook = new FacebookFactory().getInstance();
+// Use default values for oauth app id.
+facebook.setOAuthAppId("chabibah.isah@gmail.com", "Isah12345");
+// Get an access token from: 
+// https://developers.facebook.com/tools/explorer
+// Copy and paste it below.
+String accessTokenString = "EAAGiz00KzCYBAEwWdJCkPqRnUx4aYvoDYj9AKCt1EJZATU7ArdMx94b3NPqq4rsI5MHsuRXaEY09E8xImgMjkPvGbpQ8ZBuvpAT4FT3tgvocQ4PP4Xz0ZAzCAKYZBOAXOZBp9p0Mh2ligx2fh2BAbUCbtGGBOKdWzaXMenuA0w05ZAr83O8ok11ZADb3eZA2vqjIrZAY8AtpC4k1qh1Jm6aZBA";
+AccessToken at = new AccessToken(accessTokenString);
+// Set access token.
+facebook.setOAuthAccessToken(at);
+
+ // We're done.
+// Access group feeds.
+// You can get the group ID from:
+// https://developers.facebook.com/tools/explorer
+
+ // Set limit to 25 feeds.
+
+ ResponseList< Post >  feeds = facebook.getFeed("136989020831046",
+            new Reading().limit(25));
+        // For all 25 feeds...
+        for (int i = 0; i < feeds.size(); i++) {
+            // Get post.
+            Post post = feeds.get(i);
+            // Get (string) message.
+            String message = post.getMessage();
+                            // Print out the message.
+            System.out.println(message);
+            PagableList comments = post.getComments();
+            String date = post.getCreatedTime().toString();
+           // String name = post.getFrom().getName();
+            String id = post.getId();
+             System.out.println(date);
+             System.out.println(id);
+        }
+   }	
+ }
